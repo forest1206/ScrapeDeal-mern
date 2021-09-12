4507dc9df37d7f196063afc1d8ff6c0093ac1eb3
smartdev1206 <tree1206@outlook.com>
2021-09-12 09:46:56 +0700

    apollo-server-express setup

---
+++ backend/schemas/index.js
@@ -0,0 +1,28 @@
+// Schema definition
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  # A library has a branch and books
+  type Library {
+    branch: String!
+    books: [Book!]
+  }
+
+  # A book has a title and author
+  type Book {
+    title: String!
+    author: Author!
+  }
+
+  # An author has a name
+  type Author {
+    name: String!
+  }
+
+  # Queries can fetch a list of libraries
+  type Query {
+    libraries: [Library]
+  }
+`;
+
+export default typeDefs;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ import morgan from "morgan";
 import cors from "cors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import { ApolloServer, gql } from "apollo-server-express";
+import http from "http";
+
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 
 import productRoutes from "./routes/productRoutes.js";
 import bidRoutes from "./routes/bidRoutes.js";
@@ -14,45 +18,64 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import dashboardRoutes from "./routes/dashboard.js";
 
+import typeDefs from "./schemas/index.js";
+import resolvers from "./resolvers/index.js";
+
 dotenv.config();
+const port = process.env.PORT || 8000;
+const isProduction = process.env.NODE_ENV === "production";
 
-connectDB();
+async function startServer(typeDefs, resolvers) {
+  connectDB();
 
-const app = express();
+  const app = express();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  if (!isProduction) {
+    app.use(morgan("dev"));
+  }
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use("/api/product", productRoutes);
+  app.use("/api/bid", bidRoutes);
+  app.use("/api/category", categoryRoutes);
+  app.use("/api/user", userRoutes);
+  app.use("/api/upload", uploadRoutes);
+  app.use("/api/dashboard", dashboardRoutes);
+
+  app.get("/api/config/paypal", (req, res) =>
+    res.send(process.env.PAYPAL_CLIENT_ID)
+  );
+
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+
+  const httpServer = http.createServer(app);
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, path: "/graphql" });
+
+  app.use(notFound);
+  app.use(errorHandler);
+  await new Promise((resolve) => httpServer.listen({ port }, resolve));
+
+  console.log(`httpServer`, httpServer);
+  console.log(
+    `Graphql server ready http://localhost:${port}${apolloServer.graphqlPath}`
+  );
+  console.log(
+    `🚀 Electronic Shop Server running in ${process.env.NODE_ENV} mode on port ${port}`
+      .yellow.bold
+  );
 }
 
-app.use(express.json());
-app.use(cors());
-app.use("/api/product", productRoutes);
-app.use("/api/bid", bidRoutes);
-app.use("/api/category", categoryRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/upload", uploadRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-
-app.get("/api/config/paypal", (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
-
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(
-  PORT,
-  console.log(
-    `Electronic Shop Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-      .yellow.bold
-  )
-);
+startServer(typeDefs, resolvers);
